29b001eb302e3d5ec6fbe420c27c1cd2028941cd
illyanda <andregervasio97dev@gmail.com>
2025-01-03 23:22:10 -0300

    removed /nvim because it is on different repo. Created script for symlink

---
+++ scripts/symlinks_remake.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd ~
+if [ ! -d ./.config ]; then
+	mkdir .config
+fi
+cd ~/.config
+
+if [ -h ./nvim ]; then
+	rm ./nvim
+elif [ -d ./nvim ]; then
+	rm -rf ./nvim
+fi
+ln -s ~/dotfiles/nvim nvim
+
+if [ -h ./i3 ]; then
+	rm ./i3
+elif [ -d ./i3 ]; then
+	rm -rf ./i3
+fi
+ln -s ~/dotfiles/i3 i3
+
+if [ -h ./kitty ]; then
+	rm ./kitty
+elif [ -d ./kitty ]; then
+	rm -rf ./kitty
+fi
+ln -s ~/dotfiles/kitty kitty
+
