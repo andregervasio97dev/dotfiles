576567ed8c4dc7ad5e9ef094a304c2806ff5f25b
illyanda-note <andregervasio97dev@gmail.com>
2025-12-19 12:34:46 -0300

    changed ll to eza
    Changed the ll alias to use eza for better vizualisation

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --wraps=ls --description 'alias ll=ls'
-  ls $argv
+function ll --wraps=eza --description 'alias ll=eza --all --oneline --icons=always --sort Name --group-directories-last --total-size --long --tree'
+  command eza --all --oneline --icons=always --sort Name --group-directories-last --total-size --long --tree $argv
 end
