106382b6886c51e6433928377520f3a673dea5ee
illyanda-note <andregervasio97dev@gmail.com>
2025-12-30 21:56:56 -0300

    Fixed fish post update

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,3 +1,3 @@
 function ll --wraps=eza --description 'alias ll=eza --all --oneline --icons=always --sort Name --group-directories-last --total-size --long --tree'
-  command eza --all --oneline --icons=always --sort Name --group-directories-last --total-size --long --tree $argv
+  command eza --all --oneline --icons=always --sort Name --group-directories-last --total-size --long --tree --git-ignore $argv
 end
