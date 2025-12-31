eb30ee1d19add0fe4fae708208f90fc91d001a18
illyanda-note <andregervasio97dev@gmail.com>
2025-12-31 14:22:06 -0300

    Copied some waybar scripts

---
+++ .config/waybar/install.bash
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[34m"
+RESET="\e[39m"
+
+DEPENDENCIES=(
+	"libnotify"
+	"bluez"
+	"bluez-utils" # bluetoothctl
+	"brightnessctl"
+	"fzf"
+	"networkmanager" # nmcli
+	"pacman"-contrib # checkupdates
+	"pipewire-pulse"
+	"otf-commit-mono-nerd"
+)
+
+main() {
+	local errors=0
+	echo "Installing dependencies..."
+
+	local dependency
+	for dependency in "${DEPENDENCIES[@]}"; do
+		if pacman -Qi "$dependency" > /dev/null; then
+			printf "[%b/%b] %s\n" "$GREEN" "$RESET" "$dependency"
+		else
+			printf "[ ] %s...\n" "$dependency"
+
+			if sudo pacman -S --noconfirm "$dependency"; then
+				printf "[%b+%b] %s\n" "$GREEN" "$RESET" "$dependency"
+			else
+				printf "[%bx%b] %s\n" "$RED" "$RESET" "$dependency"
+				((errors += 1))
+			fi
+		fi
+	done
+
+	echo "Making scripts executable..."
+	chmod -v +x ./scripts/*.bash
+
+	echo "Restarting waybar..."
+	pkill waybar
+	waybar &> /dev/null &
+	disown
+
+	if ((errors > 0)); then
+		printf "\nInstallation completed with %b%d errors%b\n" "$RED" "$errors" "$RESET"
+	else
+		echo "Installation completed successfully!"
+	fi
+}
+
+main
